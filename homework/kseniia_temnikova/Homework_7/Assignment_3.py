61c5fb83d2b99055e8fef37801a2499e78960e97
KTemnikova <ks.temnikovaa@gmail.com>
2025-07-24 21:56:20 -0500

    Homework 7 completed

---
+++ homework/kseniia_temnikova/Homework_7/Assignment_3.py
@@ -0,0 +1,20 @@
+a = 'результат операции: 42'
+b = 'результат операции: 54'
+c = 'результат работы программы: 209'
+d = 'результат: 2'
+
+def index(string):
+    universal_index = string.index(':')
+    return universal_index
+
+
+def slicing(string):
+    sliced_string = string[index(string) + 1:]
+    return sliced_string.strip()
+
+def adding(string):
+    final_number = int(slicing(string)) + 10
+    return final_number
+
+
+print(adding(a), adding(b), adding(c), adding(d))
