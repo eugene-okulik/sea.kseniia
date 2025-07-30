12076599fb28ebd269e211435f0c398b51706c70
KTemnikova <ks.temnikovaa@gmail.com>
2025-07-29 22:33:14 -0500

    Homework_8 completed

---
+++ homework/kseniia_temnikova/Homework_8/Assignment_2.py
@@ -0,0 +1,45 @@
+import sys
+
+sys.set_int_max_str_digits(25000)
+
+
+def fibonachi():
+    num_previous = 0
+    num_new = 1
+    num_buffer = 0
+    count = 1
+    while True:
+       if count == 1:
+           count+=1
+           yield 0
+           continue
+       if count == 2:
+           count+=1
+           yield 1
+           continue
+       num_buffer = num_new
+       num_new = num_new + num_previous
+       num_previous = num_buffer
+       count += 1
+       yield num_new
+
+count = 0
+for number in fibonachi():
+    count += 1
+    if count == 20:
+        print(f'count ={count}, number={number}')
+        continue
+    if count == 200:
+        print(f'count ={count}, number={number}')
+        continue
+    if count == 1000:
+        print(f'count ={count}, number={number}')
+        continue
+    if count == 100000:
+        print(f'count ={count}, number={number}')
+        break
+
+
+
+
+
