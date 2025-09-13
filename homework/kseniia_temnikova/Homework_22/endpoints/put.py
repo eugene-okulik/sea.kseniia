86b16dd34a9a62895af6b58e91164514b67806f9
KTemnikova <ks.temnikovaa@gmail.com>
2025-09-12 19:33:24 -0500

    lint fix

--- homework/kseniia_temnikova/Homework_22/endpoints/put.py
+++ homework/kseniia_temnikova/Homework_22/endpoints/put.py
@@ -2,19 +2,16 @@ import allure
 import requests
 from endpoints.endpoint import Endpoint
 
+
 class PutObject(Endpoint):
 
     @allure.step('Changing an object')
-    def change_object_completely(self, id, payload, headers = None):
+    def change_object_completely(self, id, payload, headers=None):
         headers = headers if headers else self.headers
         self.response = requests.put(
             f'{self.url}/{id}',
-            json = payload,
-            headers = headers
+            json=payload,
+            headers=headers
         )
         print(self.response.status_code)
         self.json = self.response.json()
-
-
-
-
