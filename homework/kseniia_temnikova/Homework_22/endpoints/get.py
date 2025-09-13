8693f50ad4618806da328de87b84e9f62d999469
KTemnikova <ks.temnikovaa@gmail.com>
2025-09-12 19:32:47 -0500

    lint fix

--- homework/kseniia_temnikova/Homework_22/endpoints/get.py
+++ homework/kseniia_temnikova/Homework_22/endpoints/get.py
@@ -5,12 +5,11 @@ from endpoints.endpoint import Endpoint
 
 class GetObject(Endpoint):
 
-
     @allure.step('Getting object by id')
-    def get_by_id(self, id, headers = None):
+    def get_by_id(self, id, headers=None):
         headers = headers if headers else self.headers
         self.response = requests.get(
             f'{self.url}/{id}',
-            headers = headers
+            headers=headers
         )
         self.json = self.response.json()
